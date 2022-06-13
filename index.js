63a090d858615a9538e0cfa4320f689310c78cc7
saiteja <saiteja.nandigam999@gmail.com>
2022-06-13 18:07:59 +0530

    add mongodb

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+dbURL="mongodb+srv://saiteja:123@abcd@cluster0.2oj3acb.mongodb.net/?retryWrites=true&w=majority";
+
+
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const path = require('path');
+
 app.use(express.json());
+const userRoutes = require('./server/routes/user');
+
+console.log(process.env.dbURL)
+mongoose.connect(process.env.dbURL)
+  .then(console.log("DB Connected!!"))
+  .catch(error => console.log(error));
+
+app.use(express.json());
+
 
 app.use(express.static(__dirname + "/public"));
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, '/public','index.html')));
